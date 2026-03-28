186ad72254f34a05f5fc159b21ae15d4b3c9f2e4
PatNoO <Noordh94@hotmail.se>
2026-03-28 08:50:13 +0100

    Initial Commit

---
+++ MemoryMadness-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryMadness-IOS
+//
+//  Created by Patrik Noordh on 2026-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryMadness-IOS/MemoryMadness_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMadness_IOSApp.swift
+//  MemoryMadness-IOS
+//
+//  Created by Patrik Noordh on 2026-03-28.
+//
+
+import SwiftUI
+
+@main
+struct MemoryMadness_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
